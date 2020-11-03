0a1ad44be26a32fdb2bdaf59d3e5612fee864c3b
Alex <alex@localhost.localdomain>
2020-11-02 21:08:46 -0300

    Refatoração para Inclusão do CrossOrigin

--- ClientesSpringBoot/src/main/java/br/com/clientes/restservice/ClientesController.java
+++ ClientesSpringBoot/src/main/java/br/com/clientes/restservice/ClientesController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import br.com.clientes.service.ClienteService;
 
 @RestController
 @RequestMapping(value="/api/clientes/v1")
+@CrossOrigin("*")
 public class ClientesController {
 
 	private final ClienteService service;
